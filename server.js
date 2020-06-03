0784e025868339f7d7be781b5d056508c7c3fe9e
TEYI <tonysson7@gmail.com>
2020-06-03 15:15:58 +0200

    update server.js

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ const app = require('./app');
 const DB = process.env.DATABASE
 
 mongoose.connect(DB, {
-  useNewUrlParser:true,
-  useCreateIndex:true,
-  useFindAndModify:false,
+   useNewUrlParser:true,
+   useCreateIndex:true,
+   useFindAndModify:false,
   useUnifiedTopology: true
 }).then(result => {
   //console.log(result.connections);
